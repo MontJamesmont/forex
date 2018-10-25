9f918724e452768db7016311e7f19e9b2c22cff7
sejsmont@huboftech.io <sejsmont@huboftech.io>
2018-10-25 14:35:53 +0200

    service ready and show data in view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MomentModule } from 'angular2-moment';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ApiService } from './services/api.service';
+import { httpInterceptorProviders } from './http-interceptors';
 
 @NgModule({
   declarations: [
@@ -12,9 +18,15 @@ import { CarouselComponent } from './carousel/carousel.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    MomentModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    httpInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../services/api.service';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.sass']
 })
 export class CarouselComponent implements OnInit {
+  exchangeValue: number;
+  exchangeTime: Date;
+  exchangePairs: { name: string, symbols: string[], src: string }[];
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) {
 
-  ngOnInit() {
+    this.exchangePairs = [
+      { name: 'gbp-eur', symbols: ['GBP', 'EUR'], src: '../../assets/carousel/england.jpg' },
+      { name: 'chf-usd', symbols: ['CHF', 'USD'], src: '../../assets/carousel/Szwajcaria.jpg' },
+      { name: 'usd-gbp', symbols: ['USD', 'GBP'], src: '../../assets/carousel/USA.jpg' }
+    ];
   }
 
+  ngOnInit(): void {
+    this.getExchangeValue(this.exchangePairs[0].symbols);
+  }
+
+  getExchangeValue(symbols: string[]): void {
+    this.apiService.getLatest(symbols).subscribe((data) => {
+      this.exchangeValue = Math.round((data.rates[symbols[0]] / data.rates[symbols[1]]) * 10000) / 10000;
+      this.exchangeTime = new Date(data.timestamp * 1000);
+    });
+  }
+
+  refreshValue($event) {
+    const currentPair = this.exchangePairs[$event.current[$event.current.length - 1]];
+    this.getExchangeValue(currentPair.symbols);
+  }
 }
